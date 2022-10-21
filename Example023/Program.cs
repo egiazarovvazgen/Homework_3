82467f869130d250314f8627c3ba919e239c8cef
Vazgen <egiazarov.vazgen@icloud.com>
2022-10-21 16:11:11 +0300

    Example023

---
+++ Example023/Program.cs
@@ -0,0 +1,31 @@
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+Console.Clear();
+Console.WriteLine();
+
+Console.Write("Введите число: ");
+string writeN = Console.ReadLine();
+int N = Convert.ToInt32(writeN);
+
+int[] GetSqrtTable(int N)
+{
+    int[] SqrtArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        SqrtArray[i] = (i + 1) * (i + 1) * (i + 1);
+    }
+    return SqrtArray;
+}
+
+void PrintTable(int[] Array)
+{
+    int number = 1;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.WriteLine($"{number} ^ 3 = {Array[i]}");
+        number++;
+    }
+}
+
+int[] SqrtTable = GetSqrtTable(N);
+PrintTable(SqrtTable);
